49751366284fe987678b4577dd75a253d9fc70f8
restubayu86 <mbcregency.3a@gmail.com>
2026-08-22 12:16:32 +0700

    transkasi: update penerimaan

---
+++ app/Modules/Warehouse/Database/Migrations/2026_08_22_000001_AddQtyFieldsToChemicalStockMovements.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class AddQtyFieldsToChemicalStockMovements extends Migration
+{
+    public function up()
+    {
+        $this->forge->addColumn('chemical_stock_movements', [
+            'qty_unit' => [
+                'type'       => 'DECIMAL',
+                'constraint' => '12,2',
+                'default'    => 0,
+                'after'      => 'quantity_in',
+            ],
+            'qty_berat' => [
+                'type'       => 'DECIMAL',
+                'constraint' => '12,2',
+                'default'    => 0,
+                'after'      => 'qty_unit',
+            ],
+        ]);
+    }
+
+    public function down()
+    {
+        $this->forge->dropColumn('chemical_stock_movements', ['qty_unit', 'qty_berat']);
+    }
+}
